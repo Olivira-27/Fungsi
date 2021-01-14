8768d517869395587ea0999f534a872ba3049723
Olivira-27 <oliviraadi@gmail.com>
2021-01-14 19:40:41 +0700

    latihan 2

---
+++ src/fungsi/Fungsi1.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fungsi;
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class Fungsi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner masukan = new Scanner(System.in);
+        int pilihan = 9 ;
+        do{
+            System.out.println("Menu Luas Bangun");
+            System.out.println("1. Menghitung Luas Persegi");
+            System.out.println("2. Menghitung Luas Persegi Panjang");
+            System.out.println("3. Keluar");
+            System.out.print("Masukan Pilihan Anda : ");
+            pilihan = masukan.nextInt();
+        switch(pilihan) {
+            case 1 : luasPersegi() ; break ;
+            case 2 : luasPersegiPanjang() ; break ;
+        }
+        }while(pilihan !=3) ;
+                
+    }
+    
+
+    private static void luasPersegi() {
+        Scanner masukan = new Scanner(System.in);
+            float sisi,luas;
+            System.out.print("Masukan Nilai Sisi : ");
+            sisi = masukan.nextFloat();
+            luas = sisi*sisi;
+            System.out.println("Luas Persegi adalah : " + luas);
+    
+    }
+
+    private static void luasPersegiPanjang() {
+        Scanner masukan = new Scanner(System.in);
+            float sisi,luas;
+            System.out.print("Masukan Nilai Sisi : ");
+            sisi = masukan.nextFloat();
+            luas = sisi*sisi;
+            System.out.println("Luas Persegi adalah : " + luas);
+        
+    }
+}
+
+
+    
